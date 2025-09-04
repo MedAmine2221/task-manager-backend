3aecab35e4d2b384f5419e47b85bbdf1ba076fdc
MedAmine2221 <lazregamine258@gmail.com>
2025-09-04 23:42:49 +0200

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,27 @@
+// import { Module } from "@nestjs/common";
+// import { TypeOrmModule } from "@nestjs/typeorm";
+// import { UserController } from "./controllers/user.controller";
+// import { User } from "./entities/user.entity";
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host:'localhost',
+//       port:5432,
+//       username: 'postgres',
+//       password: 'admin0000',
+//       database: 'task-manager',
+//       entities: [User],
+//       synchronize: true
+//     }),
+//     TypeOrmModule.forFeature([User]),
+//   ],
+//   controllers: [UserController]
+// })
+// export class AppModule {}
+
+
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserController } from "./controllers/user.controller";
@@ -5,18 +29,19 @@ import { User } from "./entities/user.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host:'localhost',
-      port:5432,
-      username: 'postgres',
-      password: 'admin0000',
-      database: 'task-manager',
-      entities: [User],
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL, // ✅ Railway fournit cette variable
+        ssl: {
+          rejectUnauthorized: false, // ✅ nécessaire pour Railway
+        },
+        entities: [User],
+        synchronize: true, // ⚠️ désactiver en prod
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class AppModule {}
